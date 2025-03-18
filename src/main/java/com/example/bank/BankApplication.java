5aba768611025a241066decc769f64c7937c98d5
SagarPradhan85211 <sagarpradhan781@gmail.com>
2025-03-18 22:53:24 +0530

    project update

--- src/main/java/com/example/bank/BankApplication.java
+++ src/main/java/com/example/bank/BankApplication.java
@@ -3,21 +3,11 @@ package com.example.bank;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.sentry.Sentry;
-
 @SpringBootApplication
 public class BankApplication {
 
-    public static void main(String[] args) {
-        // Initialize Sentry before starting the application
-        Sentry.init(options -> options.setDsn("https://c227b6eb177da1c255c148f0dd0dc9b2@o4508290930900992.ingest.us.sentry.io/4508998023118848"));
+	public static void main(String[] args) {
+		SpringApplication.run(BankApplication.class, args);
+	}
 
-        try {
-            SpringApplication.run(BankApplication.class, args);
-        } catch (Exception ex) {
-            // Capture any exceptions that occur during the startup process
-            Sentry.captureException(ex);
-            System.exit(1); // Exit with error code if application fails to start
-        }
-    }
 }
